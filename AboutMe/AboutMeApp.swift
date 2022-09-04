6a51c97e1017bd91b33762c1b5a792fdf88f0ebd
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-04 22:24:36 +0300

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Amby on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
